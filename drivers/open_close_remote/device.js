f25ee0e9b799e4f0e919f01589a338926f526483
Lucas van der Have <lucasvdhave@gmail.com>
2019-08-21 13:53:58 +0200

    Add open close remote driver

--- drivers/open_close_remote/device.js
+++ drivers/open_close_remote/device.js
@@ -6,11 +6,11 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
  * TODO: what happens if the device gets switched externally, does it send a report to Homey?
  */
 class ControlOutlet extends ZigBeeDevice {
-    onMeshInit() {
+	onMeshInit() {
 
-        // Register onoff capability
-        this.registerCapability('onoff', 'genOnOff');
-    }
+		// Register onoff capability
+		this.registerCapability('onoff', 'genOnOff');
+	}
 }
 
 module.exports = ControlOutlet;
